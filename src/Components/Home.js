59e0688aba10f20abea14305064dae09294d45ba
Marie Knowlton <mtknowlton67@gmail.com>
2022-04-06 15:44:31 -0700

    Made changes t olanding-page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,27 +4,20 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-      <div>
-        <img
-          className="logo-img"
+      <div className="home-wrapper">
+        <img className="logo"
           src="https://pluspng.com/img-png/avengers-logo-png-avengers-logo-png-1376.png"
           alt="Avengers logo"
         />
         <h4>Danger this is classified information!</h4>
-        <Link
-          className="btn btn-outline-light btn-lg"
-          role="button"
-          to="/avengers"
-        >
-          ENTER
-        </Link>
-      </div>
-      <div className="home-wrapper">
-        <img
-          className="home-img"
-          src="https://st3.depositphotos.com/2927373/18205/i/450/depositphotos_182059154-stock-photo-prague-czech-republic-january-colorful.jpg"
+        <Link to="/avengers">
+          
+        
+          <img className="comic-pic"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRiNNvzOK0CdeSYl-j1UUf5XgBTwj6iqzO_Q&usqp=CAU"
           alt="Avengers comic book"
         />
+        </Link>
       </div>
     </>
   );
